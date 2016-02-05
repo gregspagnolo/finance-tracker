0788941b5c7287b635d3c9526640104a4fde0899
gregspagnolo <dwdrummer@gmail.com>
2016-02-04 22:11:17 -0600

    update views and navigation

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -6,6 +6,5 @@ class User::RegistrationsController < Devise::RegistrationsController
   def configure_permitted_parameters
    devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name)
    devise_parameter_sanitizer.for(:account_update).push(:first_name, :last_name)
-   
   end
 end
